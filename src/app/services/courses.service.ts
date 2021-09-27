a6558e7ce98d22e43f0a7885a8d5ef790d78b160
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-27 11:43:16 -0500

    Adding course service with load function

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,19 @@
+import { map, shareReplay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Course } from './../model/course';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  constructor(private http: HttpClient) {}
+
+  loadCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>('/api/courses').pipe(
+      map((response: any) => response['payload']),
+      shareReplay()
+    );
+  }
+}
