720887b1e42b13aec9143597dc00a2d31215bbd0
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-27 11:43:50 -0500

    Creating component course card list

---
+++ src/app/course-card-list/course-card-list.component.ts
@@ -0,0 +1,26 @@
+import { Course } from './../model/course';
+import { Component, Input } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
+
+@Component({
+  selector: 'app-course-card-list',
+  templateUrl: './course-card-list.component.html',
+  styleUrls: ['./course-card-list.component.scss']
+})
+export class CourseCardListComponent {
+  @Input() courses: Course[] = [];
+
+  constructor(private dialog: MatDialog) { }
+
+  editCourse(course: Course) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '400px';
+    dialogConfig.data = course;
+
+    this.dialog.open(CourseDialogComponent, dialogConfig);
+  }
+}
