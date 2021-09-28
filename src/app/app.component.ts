4fb23d91b62e437e7428bbffb087e126f336bd35
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-28 15:39:06 -0500

    adding logout logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { AuthStoreService } from './services/auth.store.service';
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(public authService: AuthStoreService) {}
 
   ngOnInit() {}
 
-  logout() {}
+  logout() {
+    this.authService.logout();
+    localStorage.clear();
+  }
 }
