3a6a776f38938aa06b734d06739bca04d45bbbde
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-28 14:27:57 -0500

    Refactor save function

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,5 +1,4 @@
-import { LoadingService } from './../loading/loading.service';
-import { CourseService } from './../services/courses.service';
+import { StoreService } from './../services/store.service';
 import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Course } from '../model/course';
@@ -9,7 +8,7 @@ import * as moment from 'moment';
 @Component({
   selector: 'app-course-dialog',
   templateUrl: './course-dialog.component.html',
-  styleUrls: ['./course-dialog.component.css']
+  styleUrls: ['./course-dialog.component.css'],
 })
 export class CourseDialogComponent implements OnInit, AfterViewInit {
   form: FormGroup;
@@ -19,9 +18,10 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
     @Inject(MAT_DIALOG_DATA) course: Course,
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
-    private courseService: CourseService,
-    private loading: LoadingService
-  ) { this.course = course; }
+    private store: StoreService
+  ) {
+    this.course = course;
+  }
 
   ngOnInit(): void {
     this.initForm();
@@ -30,10 +30,8 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {}
 
   save() {
-    const courseSaved$ = this.courseService.save(+this.course.id, this.form.value);
-    const loadingCourses$ = this.loading.showLoaderUntilCompleted(courseSaved$);
-
-    loadingCourses$.subscribe((value: any) => this.close(value));
+    this.store.saveCourses(+this.course.id, this.form.value)
+      .subscribe(() => this.close(this.form.value));
   }
 
   close(value?: any) {
