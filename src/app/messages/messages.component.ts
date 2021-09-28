2e8ed1d5ba04465b000c2979bdc5d7cd07c23342
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-28 10:15:28 -0500

    Adding logic into messages component, creating html and styles

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,29 +1,24 @@
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import {Message} from '../model/message';
-import {tap} from 'rxjs/operators';
+import { MessageService } from './messages.service';
 
 @Component({
-  selector: 'messages',
+  selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  showMessages = false;
+  error$: Observable<string[]>;
 
-
-  constructor() {
-
-  }
+  constructor(private messageService: MessageService) {}
 
   ngOnInit() {
-
-
+    this.error$ = this.messageService.error$.pipe(tap(() => this.showMessages = true));
   }
-
 
   onClose() {
-
-
+    this.showMessages = false;
   }
-
 }
