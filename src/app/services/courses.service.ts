638cad7279b8cd4cc4ea95e81477668fe02752bf
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-28 14:28:32 -0500

    Adding error handling

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,24 +1,35 @@
-import { map, shareReplay } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { catchError, map, shareReplay } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 import { Course } from './../model/course';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MessageService } from '../messages/messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   loadCourses(): Observable<Course[]> {
     return this.http.get<Course[]>('/api/courses').pipe(
       map((response: any) => response['payload']),
-      shareReplay()
+      catchError((error: any) => {
+        this.messageService.showErrors('Could not load courses');
+        return throwError(error);
+      })
     );
   }
 
   save(courseId: number, changes: Partial<Course>): Observable<any> {
     return this.http.put(`/api/courses/${courseId}`, changes).pipe(
+      catchError((error) => {
+        this.messageService.showErrors('Could not save course');
+        return throwError(error);
+      }),
       shareReplay()
     );
   }
