6d6dd55245497ca3a12d4ffc1a19f3bda2ce0954
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-28 10:14:27 -0500

    Adding better indentation to backend

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -1,46 +1,40 @@
-
-
-
-import {Request, Response} from 'express';
-import {LESSONS} from "./db-data";
-import {setTimeout} from "timers";
-
-
+import { Request, Response } from 'express';
+import { LESSONS } from './db-data';
+import { setTimeout } from 'timers';
 
 export function searchLessons(req: Request, res: Response) {
+  let lessons: any;
+  const queryParams = req.query;
+  const courseId = queryParams.courseId,
+    filter = queryParams.filter || '',
+    sortOrder = queryParams.sortOrder || 'asc',
+    pageNumber = +queryParams.pageNumber || 0,
+    pageSize = +queryParams.pageSize || 3;
 
-    const queryParams = req.query;
+  if (courseId) {
+    lessons = Object.values(LESSONS)
+      .filter((lesson) => lesson.courseId === courseId)
+      .sort((l1, l2) => l1.id - l2.id);
+  } else {
+    lessons = Object.values(LESSONS);
+  }
 
-    const courseId = queryParams.courseId,
-          filter = queryParams.filter || '',
-          sortOrder = queryParams.sortOrder || 'asc',
-          pageNumber = parseInt(queryParams.pageNumber) || 0,
-          pageSize = parseInt(queryParams.pageSize) || 3;
+  if (filter) {
+    lessons = lessons.filter(
+      (lesson: any) =>
+        lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >=
+        0
+    );
+  }
 
-    let lessons;
+  if (sortOrder === 'desc') {
+    lessons = lessons.reverse();
+  }
 
-    if (courseId) {
-        lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
-    }
-    else {
-        lessons = Object.values(LESSONS);
-    }
+  const initialPos = pageNumber * pageSize;
+  const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
 
-    if (filter) {
-       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
-    }
-
-    if (sortOrder == "desc") {
-        lessons = lessons.reverse();
-    }
-
-    const initialPos = pageNumber * pageSize;
-
-    const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
-
-    setTimeout(() => {
-        res.status(200).json({payload: lessonsPage});
-    },1000);
-
-
+  setTimeout(() => {
+    res.status(200).json({ payload: lessonsPage });
+  }, 1000);
 }
