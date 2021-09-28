e221cf538f2e0897be21e8ff81d57d4d5895819a
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-28 15:38:44 -0500

    adding auth logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,39 +1,40 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { AuthStoreService } from './../services/auth.store.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'login',
+  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private router: Router) {
-
-    this.form = fb.group({
-      email: ['test@angular-university.io', [Validators.required]],
-      password: ['test', [Validators.required]]
-    });
-
-  }
+    private router: Router,
+    private authService: AuthStoreService) { }
 
   ngOnInit() {
-
+    this.initForm();
   }
 
   login() {
-
     const val = this.form.value;
 
-
-
+    this.authService.login(val.email, val.password)
+      .subscribe(
+        () => this.router.navigateByUrl('/courses'),
+        (error) => alert('Login failed')
+      );
   }
 
+  private initForm(): void {
+    this.form = this.fb.group({
+      email: ['test@angular-university.io', [Validators.required]],
+      password: ['test', [Validators.required]],
+    });
+  }
 }
