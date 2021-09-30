202722565cebfbb46d549452fa46e3d729bf084b
Andres Alban Carvallo <bethesdas101@gmail.com>
2021-09-30 10:37:33 -0500

    Adding better indentation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,47 +1,10 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {
-  concat,
-  fromEvent,
-  interval,
-  noop,
-  observable,
-  Observable,
-  of,
-  timer,
-  merge,
-  Subject,
-  BehaviorSubject,
-  AsyncSubject,
-  ReplaySubject, from
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-
-
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent implements OnInit {
-
-    ngOnInit() {
-
-
-    }
-
-
-  run() {
-
-
-
-  }
-
-
+export class AboutComponent {
+  run() {}
 }
-
-
-
-
-
-
